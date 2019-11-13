cddb164a048b113279a10bf901cf4eebf4b9965b
Alexey Drozdov <Oleksiy.Drozdov@iwedia.com>
2019-11-13 09:33:39 +0200

    backtrace files added

---
+++ sagemath/spherical_oscillator/backtrace.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <backtrace.h>
+
+#define ARR_SIZE 50
+#define BACKTRACE_SIZ 64
+
+void show_backtrace(void)
+{
+    void    *array[BACKTRACE_SIZ];
+    size_t   size, i;
+    char   **strings;
+
+    size = backtrace(array, BACKTRACE_SIZ);
+    strings = backtrace_symbols(array, size);
+
+    for (i = 0; i < size; i++) {
+        printf("%p : %s\n", array[i], strings[i]);
+    }
+
+    free(strings);  // malloced by backtrace_symbols
+}
+
+void catch_signal(int sig)
+{
+	printf("Show backtrace Segmentation fault\n");
+	show_backtrace();
+	exit(0);
+}
---
+++ sagemath/spherical_oscillator/backtrace.h
@@ -0,0 +1,2 @@
+#include <signal.h>
+void catch_signal(int sig);

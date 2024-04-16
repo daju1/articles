c136694460f3c8b24dceb5b7978930b7d32cf842
Alexey Drozdov <daju@yandex.ru>
2024-04-16 10:41:08 +0000

    casimir_honeycomb_drive bibtex

--- casimir/Casimir_honeycomb_drive/tex2pdf.sh
+++ casimir/Casimir_honeycomb_drive/tex2pdf.sh
@@ -1,3 +1,18 @@
-xelatex Casimir_honeycomb_drive.tex
-pdflatex Casimir_honeycomb_drive.tex
+fn=Casimir_honeycomb_drive
+
+rm $fn.aux
+rm $fn.dvi
+rm $fn.out
+rm $fn.log
+rm $fn.bib
+rm $fn.bbl
+
+xelatex $fn.tex  # generates aux,dvi etc
+cp $fn.aux $fn.tex.aux
+
+bibtex $fn.tex # generates a bbl
+cp $fn.tex.bbl $fn.bbl
+
+xelatex $fn.tex
+pdflatex $fn.tex
 

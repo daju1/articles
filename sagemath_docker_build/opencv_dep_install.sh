5b417eb7ddeb5eaee60cdc008d1b83ec260552ef
Alexey Drozdov <daju@yandex.ru>
2025-01-23 20:39:17 +0000

    opencv_dep_install.sh added

---
+++ sagemath_docker_build/opencv_dep_install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y python3-opencv python3-pip
+
+# pip3 install opencv-python
+# pip3 install opencv-contrib-python
+
+
+/usr3/articles/sagemath_docker_build/sage/local/bin/python3 -m pip install opencv-python opencv-contrib-python # vtkplotlib numpy-stl

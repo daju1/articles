1e61b83db4ee63920c8de61ede10b7f7bcb5be4f
Alexey Drozdov <daju@yandex.ru>
2025-01-23 20:40:44 +0000

    vtkplotlib_dep_install.sh added

---
+++ sagemath_docker_build/vtkplotlib_dep_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y python3-pip
+
+# pip3 install vtkplotlib
+# pip3 install stl
+
+/usr3/articles/sagemath_docker_build/sage/local/bin/python3 -m pip install vtkplotlib numpy-stl

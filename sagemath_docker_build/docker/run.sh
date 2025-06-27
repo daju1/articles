6628b0f548ea9f785197f7287c8c7c6fd61a6643
Alexey Drozdov <daju@yandex.ru>
2025-06-27 19:16:35 +0000

    run.sh update

--- sagemath_docker_build/docker/run.sh
+++ sagemath_docker_build/docker/run.sh
@@ -10,15 +10,22 @@ DOCKER_IMAGE=ubuntu_sage_build:18.04
 PROJECTS_DIR=$(dirname $(dirname ${PWD}))
 USR3_DIR=$(dirname $(dirname $(dirname ${PWD})))
 
+docker network create sagemath
+
 docker run -it --rm --name sage_build_container --cap-add=NET_ADMIN --device /dev/net/tun \
     --workdir=${PWD} \
     -p 8880:8888 \
+    --network sagemath \
     -v /home/${USER}/.local:/home/${USER}/.local \
     -v ${USR3_DIR}/winlibghemical:${PROJECT_ROOT}/winlibghemical \
     -v ${USR3_DIR}/moldyn:${PROJECT_ROOT}/moldyn \
     -v ${USR3_DIR}/science:${PROJECT_ROOT}/science \
     -v ${USR3_DIR}/study:${PROJECT_ROOT}/study \
+    -v ${USR3_DIR}/tss_bot:${PROJECT_ROOT}/tss_bot \
+    -v ${USR3_DIR}/jenkins:${PROJECT_ROOT}/jenkins \
     -v ${USR3_DIR}/test:${PROJECT_ROOT}/test \
+    -v ${USR3_DIR}/LaTex2Docx:${PROJECT_ROOT}/LaTex2Docx \
+    -v ${USR3_DIR}/Prompt-Engineering-Guide:${PROJECT_ROOT}/Prompt-Engineering-Guide \
     -v ${PROJECTS_DIR}:${PROJECT_ROOT}/work \
     -v ${PROJECT_ROOT}:${PROJECT_ROOT} \
     -v /opt/output/target/root:/opt/output/target/root \

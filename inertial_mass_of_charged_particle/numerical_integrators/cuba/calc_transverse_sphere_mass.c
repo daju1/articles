41f245038d3f37cdcc04fb9ac2ad198cfb413038
Alexey Drozdov <daju@yandex.ru>
2025-08-20 10:29:18 +0000

    calc_transverse_sphere_mass added

---
+++ inertial_mass_of_charged_particle/numerical_integrators/cuba/calc_transverse_sphere_mass.c
@@ -0,0 +1,213 @@
+/*
+	demo-c.c
+		test program for the Cuba library
+		last modified 13 Mar 15 th
+*/
+
+#include "calc_RO.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cuba.h"
+
+
+/* Структура для передачи параметров задачи */
+typedef struct {
+    double R0;     /* Радиус сферы */
+    double rho0;   /* Расстояние от оси вращения до центра сферы */
+    double omega;  /* Угловая скорость вращения */
+    double c;      /* Скорость света */
+} ProblemParams;
+
+/* Функция для вычисления электрического поля по Лиенару-Вихерту */
+static void compute_electric_field(
+    double ra, double theta_a, double psi_a,
+    double rq, double theta_q, double psi_q,
+    const ProblemParams *params,
+    double *E_rho, double *E_phi, double *E_z, 
+    double *_R
+) {
+    /* Преобразование в декартовы координаты в локальной системе */
+    double x_a = ra * sin(theta_a) * cos(psi_a);
+    double y_a = ra * sin(theta_a) * sin(psi_a);
+    double z_a = ra * cos(theta_a);
+    
+    double x_q = rq * sin(theta_q) * cos(psi_q);
+    double y_q = rq * sin(theta_q) * sin(psi_q);
+    double z_q = rq * cos(theta_q);
+    
+    /* Преобразование в цилиндрическую систему (исходная) */
+    double rho_a = sqrt(pow(params->rho0 + x_a, 2) + pow(y_a, 2));
+    double phi_a = atan2(y_a, params->rho0 + x_a);
+    if (phi_a < 0) phi_a += 2.0 * M_PI;
+    
+    double rho_q = sqrt(pow(params->rho0 + x_q, 2) + pow(y_q, 2));
+    double phi_q = atan2(y_q, params->rho0 + x_q);
+    if (phi_q < 0) phi_q += 2.0 * M_PI;
+    
+    /* Вектор от источника к наблюдателю */
+    double R_rho = rho_a * cos(phi_a - phi_q) - rho_q;
+    double R_phi = rho_a * sin(phi_a - phi_q);
+    double R_z = z_a - z_q;
+    *_R = sqrt(pow(R_rho, 2) + pow(R_phi, 2) + pow(R_z, 2));
+    double R = *_R;
+    
+    /* Скорость и ускорение источника */
+    double v_q = rho_q * params->omega;
+    double a_q = -rho_q * pow(params->omega, 2);
+
+    //printf("v/c=%f\n", v_q/params->c);
+    
+    /* Радиус Лиенара-Вихерта */
+    double R_star = R - (R_phi * v_q) / params->c;
+    
+    /* Защита от деления на ноль */
+    if (R_star < 1e-10) {
+        *E_rho = 0.0;
+        *E_phi = 0.0;
+        *E_z = 0.0;
+        return;
+    }
+    
+    /* Основная формула Лиенара-Вихерта */
+    double common_factor = 1.0 / pow(R_star, 3);
+    double velocity_factor = 1.0 + (R_rho * a_q) / pow(params->c, 2) - pow(v_q, 2) / pow(params->c, 2);
+    
+    *E_rho = common_factor * (R_rho * velocity_factor + (a_q * R_star * R) / pow(params->c, 2));
+    //*E_rho = common_factor * ((a_q * R_star * R) / pow(params->c, 2));
+    //*E_rho = ((a_q / R) / pow(params->c, 2));
+    *E_phi = common_factor * ((R_phi - (R * v_q) / params->c) * velocity_factor);
+    *E_z = common_factor * (R_z * velocity_factor);
+}
+
+// the charge distribution
+static inline cubareal rho_q (cubareal r0, cubareal q)
+{
+    return 3 * (q) / (4*M_PI*(r0)*(r0)*(r0));
+}
+
+// интегрирование по координатам заряда источника потенциала
+// E2 - индуктивная компонента массы
+// В приближении малых скоростей ${}^{v} \big / {}_{c}\ll 1$
+// и малых ускорений $a{{r}_{0}}\ll {{c}^{2}}$ и при игнорировании запаздывания
+// Электрическое поле самоиндукции ($z$ компонента)
+/*
+\[{E}_{2}=\\
+\int\limits_{{{r}_{q}}}\int\limits_{{{\varphi}_{q}}}\int\limits_{{{\theta}_{q}}}\\
+{\left\{ -\frac{{a_z}}{{{c}^{2}}{{{R}_{0}}}} \right\}\\
+{\rho \left( {{r}_{q}} \right){{r}_{q}}^{2}\sin \left( {{\theta }_{q}} \right)}\ }d{{\theta }_{q}}d{{\varphi }_{q}}d{{r}_{q}}\]
+*/
+static inline cubareal Iq (const ProblemParams *params, cubareal q, cubareal psi_a, cubareal theta_a, cubareal ra, cubareal psi_q, cubareal theta_q, cubareal rq )
+{
+    cubareal r0 = params->R0;
+    // В приближении малых скоростей ${}^{v}/{}_{c}\ll 1$
+    // и малых ускорений $a{{r}_{0}}\ll {{c}^{2}}$
+    // и при игнорировании запаздывания
+    cubareal R = R0 (ra, theta_a, rq, theta_q, psi_q);
+
+    double mean_a = -params->rho0 * pow(params->omega, 2);
+
+    /* Вычисление электрического поля */
+    double E_rho, E_phi, E_z, _R = R;
+    compute_electric_field(ra, theta_a, psi_a, rq, theta_q, psi_q, params, &E_rho, &E_phi, &E_z, &_R);
+//printf("R = %e _R = %e\n", R, _R);
+//printf("1/R = %e E_rho = %e\n", 1.0/R, E_rho);
+    return rho_q(r0, q) * Sq(rq) * sin(theta_q) * E_rho;
+    return mean_a * rho_q(r0, q) * Sq(rq) * sin(theta_q) / R / pow(params->c, 2);
+    return rho_q(r0, q) * Sq(rq) * sin(theta_q) / R;
+}
+
+// интегрирование по координатам точек наблюдения
+/*static inline*/ cubareal Ia (const ProblemParams *params, cubareal q, cubareal phi_a, cubareal theta_a, cubareal ra, cubareal phi_q, cubareal theta_q, cubareal rq)
+{
+    cubareal r0 = params->R0;
+    return rho_q(r0, q) * Sq(ra) * sin(theta_a) * Iq(params, q, phi_a, theta_a, ra, phi_q, theta_q, rq);
+}
+
+int Integrand(const int *ndim, const cubareal xx[],
+  const int *ncomp, cubareal ff[], void *userdata) {
+    ProblemParams *params = (ProblemParams *)userdata;
+
+    // theta_a, ra, phi_q, theta_q, rq
+#if 0
+    #define psi_a   xx[0]
+    #define theta_a xx[1]
+    #define ra      xx[2]
+    #define psi_q   xx[3]
+    #define theta_q xx[4]
+    #define rq      xx[5]
+#else
+    #define psi_a 0
+    #define theta_a xx[0]
+    #define ra      xx[1]
+    #define psi_q   xx[2]
+    #define theta_q xx[3]
+    #define rq      xx[4]
+#endif
+    #define observed_ratio ff[0]
+
+    if ( fabs(theta_a - theta_q) < 1e-12 && fabs(ra - rq) < 1e-12 && fabs(psi_q - psi_a) < 1e-12)
+    {
+        printf ("theta_a = %e ", theta_a);
+        printf ("theta_q = %e ", theta_q);
+        printf ("ra = %e ", ra);
+        printf ("rq = %e ", rq);
+        printf ("psi_a = %e\n", psi_a);
+        printf ("psi_q = %e\n", psi_q);
+
+        return -999;
+    }
+
+    cubareal r0 = params->R0;
+    cubareal q = 1.0;
+
+    /* Вычисление ускорения */
+    double Gamma = -params->rho0 * pow(params->omega, 2);
+
+    double f = r0 * r0 * (2 * M_PI) * M_PI * (2*M_PI) * M_PI * Ia (params, q, psi_a * (2*M_PI), theta_a * M_PI, ra * r0, psi_q * (2*M_PI), theta_q * M_PI, rq * r0 );
+
+    /*
+        Твою энегрию электрического поля для сравнения с моим результатом самодействия я умножил на 2
+        double U = 3.0 / (5.0 * params.R0);     // Энергия электрического поля 
+        double m_perp_B = 2 * U / (params.c * params.c);  // Вариация типа В (теоретическое значение) 
+        Потому что никто за сто с лишним лет на это не обращает внимание, но даже у Ферми есть ошибка в строке 
+        ```
+            или, заменив $O$ на $Р'$ (что ничего не изменяет) и взяв полусумму двух полученных таким образом значений,
+
+            $$\frac{1}{2} \int \int\frac{\overrightarrow{P-P'}}{r^3} \, \frac{\vec {Г} \cdot \overrightarrow{\left(P - P'\right)}}{c^2} \, de\,de'.$$
+        ```
+        Ошибка в том, что при вычислении самодействия вычисляя например силу электромагнитной индукции с помощью которой кусочек одного и того же зарадя 1 действует на кусочек того же заряда 2 мы не должны брать полусумму, потому что сила взаимоиндукции ЭМ поля (вызванная ускорением кусочка заряда 2) которая действует на кусочек заряда 1 и точно так же сила взаимоиндукции ЭМ поля (вызванная ускорением кусочка заряда 1) которая действует на кусочек заряда 2 они обе должны суммироваться в общий интеграл самодействия.
+
+        Что касается множителя 1/2 который вычислячется при вычислении электромагнитного поля например в параграфах 15, 16 у И.Е.Тамма то (мы можем позже отдельно проанализировать это) но мне кажется что для здесь этот множитель взят законно. А при вычислении силы самодействия он берётся незаконно. Если анализировать дальше следствия которые вытекают из этого факта, то нам придётся очень глубоко углубляться в теорию.
+
+        Кстати косвенным указанием моей правоты насчёт этого коэффициента 1/2 является цитата из русской Википедии
+
+        Русская википедия в статье "Классический радиус электрона"  на момент написания данной работы даёт следующее определение:
+
+        ```
+            Классический радиус электрона равен радиусу полой сферы, на которой равномерно распределён заряд, если этот заряд равен заряду электрона, а потенциальная энергия электростатического поля ${U}_{0}$ полностью эквивалентна половине массы электрона (без учета квантовых эффектов):
+
+            ${\displaystyle U_{0}={\frac {1}{2}}{\frac {1}{4\pi \varepsilon _{0}}}\cdot {\frac {e^{2}}{r_{0}}}={\frac {1}{2}}m_{0}c^{2}}$.
+        ```
+
+        Возникает закономерный вопрос: а чему соответствует вторая половина массы электрона?
+
+        Но об этом мы потом поговорим - это очень большой разговор, сейчас я просто обьясняю свои потивы почему я в код твоей программы добавляю множитель 2
+    */
+
+    double U = 3.0 / (5.0 * params->R0);     /* Энергия электрического поля */
+    double m_perp_B = 2 * U; /* Вариация типа В (теоретическое значение) */
+
+    /* Вычисление поперечной массы */
+    //double m_perp_A = integral[0] / Gamma / (params.c * params.c);  /* Вариация типа А */
+    double m = f/Gamma/ (params->c * params->c);
+
+    /* Проверка коэффициента 4/3 */
+    double expected_ratio = 4.0 / 3.0;
+    observed_ratio = m / m_perp_B;
+
+    return 0;
+}
+
---
+++ inertial_mass_of_charged_particle/numerical_integrators/cuba/main_transverse_sphere_mass.c
@@ -0,0 +1,141 @@
+#define INCLUDE_CUBA_H
+#include "calc_RO.h"
+
+int Integrand(const int *ndim, const cubareal xx[],
+  const int *ncomp, cubareal ff[], void *userdata);
+
+/*********************************************************************/
+#if 0
+#define NDIM 6
+#else
+#define NDIM 5
+#endif
+#define NCOMP 1
+#define USERDATA NULL
+#define NVEC 1
+#define EPSREL 1e-3
+#define EPSABS 1e-12
+#define VERBOSE 2
+#define LAST 4
+#define SEED 0
+#define MINEVAL 0
+#define MAXEVAL 500000
+
+#define NSTART 1000
+#define NINCREASE 500
+#define NBATCH 1000
+#define GRIDNO 0
+#define STATEFILE NULL
+#define SPIN NULL
+
+#define NNEW 1000
+#define NMIN 2
+#define FLATNESS 25.
+
+#define KEY1 47
+#define KEY2 1
+#define KEY3 1
+#define MAXPASS 5
+#define BORDER 0.
+#define MAXCHISQ 10.
+#define MINDEVIATION .25
+#define NGIVEN 0
+#define LDXGIVEN NDIM
+#define NEXTRA 0
+
+#define KEY 0
+
+/* Структура для передачи параметров задачи */
+typedef struct {
+    double R0;     /* Радиус сферы */
+    double rho0;   /* Расстояние от оси вращения до центра сферы */
+    double omega;  /* Угловая скорость вращения */
+    double c;      /* Скорость света */
+} ProblemParams;
+
+
+int main() {
+  int comp, nregions, neval, fail;
+  cubareal integral[NCOMP], error[NCOMP], prob[NCOMP];
+
+    /* Параметры задачи */
+    ProblemParams params;
+
+    double v_c = 0.000001;
+    
+    /* Скорость и ускорение источника */
+    
+    params.R0 = 1;    /* радиус сферы (половина R1) */
+    params.rho0 = 10;  /* расстояние от оси вращения */
+    params.c = 1;      /* скорость света в м/с */
+    params.omega = v_c * params.c / params.rho0;  /* угловая скорость */
+
+#if 1
+  printf("-------------------- Vegas test --------------------\n");
+
+  Vegas(NDIM, NCOMP, Integrand, &params, NVEC,
+    EPSREL, EPSABS, VERBOSE, SEED,
+    MINEVAL, MAXEVAL, NSTART, NINCREASE, NBATCH,
+    GRIDNO, STATEFILE, SPIN,
+    &neval, &fail, integral, error, prob);
+
+  printf("VEGAS RESULT:\tneval %d\tfail %d\n",
+    neval, fail);
+  for( comp = 0; comp < NCOMP; ++comp )
+    printf("VEGAS RESULT:\t%.8f +- %.8f\tp = %.3f\n",
+      (double)integral[comp], (double)error[comp], (double)prob[comp]);
+#endif
+
+#if 0
+  printf("\n-------------------- Suave test --------------------\n");
+
+  Suave(NDIM, NCOMP, Integrand, USERDATA, NVEC,
+    EPSREL, EPSABS, VERBOSE | LAST, SEED,
+    MINEVAL, MAXEVAL, NNEW, NMIN, FLATNESS,
+    STATEFILE, SPIN,
+    &nregions, &neval, &fail, integral, error, prob);
+
+  printf("SUAVE RESULT:\tnregions %d\tneval %d\tfail %d\n",
+    nregions, neval, fail);
+  for( comp = 0; comp < NCOMP; ++comp )
+    printf("SUAVE RESULT:\t%.8f +- %.8f\tp = %.3f\n",
+      (double)integral[comp], (double)error[comp], (double)prob[comp]);
+#endif
+
+#if 0
+  printf("\n------------------- Divonne test -------------------\n");
+
+  Divonne(NDIM, NCOMP, Integrand, USERDATA, NVEC,
+    EPSREL, EPSABS, VERBOSE, SEED,
+    MINEVAL, MAXEVAL, KEY1, KEY2, KEY3, MAXPASS,
+    BORDER, MAXCHISQ, MINDEVIATION,
+    NGIVEN, LDXGIVEN, NULL, NEXTRA, NULL,
+    STATEFILE, SPIN,
+    &nregions, &neval, &fail, integral, error, prob);
+
+  printf("DIVONNE RESULT:\tnregions %d\tneval %d\tfail %d\n",
+    nregions, neval, fail);
+  for( comp = 0; comp < NCOMP; ++comp )
+    printf("DIVONNE RESULT:\t%.8f +- %.8f\tp = %.3f\n",
+      (double)integral[comp], (double)error[comp], (double)prob[comp]);
+#endif
+
+#if 0
+  printf("\n-------------------- Cuhre test --------------------\n");
+
+  Cuhre(NDIM, NCOMP, Integrand, USERDATA, NVEC,
+    EPSREL, EPSABS, VERBOSE | LAST,
+    MINEVAL, MAXEVAL, KEY,
+    STATEFILE, SPIN,
+    &nregions, &neval, &fail, integral, error, prob);
+
+  printf("CUHRE RESULT:\tnregions %d\tneval %d\tfail %d\n",
+    nregions, neval, fail);
+  for( comp = 0; comp < NCOMP; ++comp )
+    printf("CUHRE RESULT:\t%.8f +- %.8f\tp = %.3f\n",
+      (double)integral[comp], (double)error[comp], (double)prob[comp]);
+#endif
+
+  return 0;
+}
+

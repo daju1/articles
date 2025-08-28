42aeb4f6303544e20b037fea3ae92e33f99433da
Alexey Drozdov <daju@yandex.ru>
2025-08-28 19:00:37 +0000

    calc_longitudinal_sphere_mass computing_electric_field update with imagine integrate sphere when lorentz factor applied

--- inertial_mass_of_charged_particle/numerical_integrators/cuba/calc_longitudinal_sphere_mass.c
+++ inertial_mass_of_charged_particle/numerical_integrators/cuba/calc_longitudinal_sphere_mass.c
@@ -32,12 +32,6 @@ typedef struct {
     int use_fast_integrand;
 } ProblemParams;
 
-/* Функция для вычисления фактора Лоренца */
-static inline double lorentz_factor(double v, double c) {
-    double beta = v / c;
-    return 1.0 / sqrt(1.0 - beta * beta);
-}
-
 /* Функция для вычисления поправки Ферми */
 static double fermi_correction(
     double R_x,
@@ -160,11 +154,15 @@ static void computing_electric_field(
     /* (1.0 + Ra_v2_c2) * (1.0+fermi_factor)  = 1.0 + Ra_v2_c2_fermi */
     double Ra_v2_c2_fermi = Ra_v2_c2 + fermi_factor + fermi_factor * Ra_v2_c2;
 
-    common_factor1 *= (1.0+fermi_factor);
-
     double f_one = 1.0;
     if (use_fast_integrand)
     {
+        /*
+            Режим быстрого интегранда выключает из процедуры численного интегрирования
+            симметричную часть интегранда 
+            $$R_z / Cb(R - (R_z * v_z) / c)$$
+            которая теоретически при интегрировании по сфере должна дать значение близкое к нулю.
+        */
         f_one = 0.0;
     }
 
@@ -194,9 +192,15 @@ static void computing_electric_field(
     *E_total_z = common_factor * ( ((f_one + Ra_v2_c2_fermi) * R_z - (1.0 + Ra_v2_c2_fermi) * R*v_z/c)  - (a_z * R_star * R) / Sq(c) - (a_z * R_star * R) * fermi_factor / Sq(c));
 }
 
+/* Функция для вычисления фактора Лоренца */
+static inline double lorentz_factor(double v, double c) {
+    double beta = v / c;
+    return 1.0 / sqrt(1.0 - beta * beta);
+}
+
 /* Функция для вычисления электрического поля по Лиенару-Вихерту для продольного случая */
 static void compute_electric_field(
-    double ra, double theta_a, double phi_a,
+    double ra, double theta_a, double phi_a, /*воображаемые координаты несплюснутой сферы*/
     double rq, double theta_q, double phi_q,
     const ProblemParams *params,
     double *E1_x, double *E1_y, double *E1_z,
@@ -212,12 +216,6 @@ static void compute_electric_field(
     double y_q = rq * sin(theta_q) * sin(phi_q);
     double z_q = rq * cos(theta_q);
 
-    /* Вектор от источника к наблюдателю */
-    double R_x = x_a - x_q;
-    double R_y = y_a - y_q;
-    double R_z = z_a - z_q;
-    double R = sqrt(R_x*R_x + R_y*R_y + R_z*R_z);
-
     /* Скорость и ускорение источника */
     double v_x = 0;
     double v_y = 0;
@@ -228,9 +226,25 @@ static void compute_electric_field(
     double a_y = 0;
     double a_z = params->a;  /* Продольное ускорение */
 
+    if (params->use_lorentz_factor || params->use_lorentz_general_factor)
+    {
+        /* Учет Лоренц-сокращения в направлении движения */
+        double gamma = lorentz_factor(v_z, params->c);
+        /* Учет Лоренц-сокращения: в системе отсчета наблюдателя
+        сфера сжата вдоль направления движения (ось z) */
+        z_q = z_q / gamma;
+        z_a = z_a / gamma;
+    }
+
+    /* Вектор от источника к наблюдателю */
+    double R_x = x_a - x_q;
+    double R_y = y_a - y_q;
+    double R_z = z_a - z_q;
+    double R = sqrt(R_x*R_x + R_y*R_y + R_z*R_z);
+
     computing_electric_field(
         0,           /* текущее время */
-        0,     /* запаздывающее время */
+        0,           /* запаздывающее время */
         R,
         R_x, R_y, R_z,
         v_x, v_y, v_z,
@@ -244,6 +258,7 @@ static void compute_electric_field(
         params->use_fast_integrand
     );
 }
+
 
 /* Функция для вычисления собственного времени */
 static double compute_proper_time(
@@ -277,9 +292,7 @@ static double compute_proper_time(
 static void get_source_position(
     double t_prime,
     double t0,
-    double rq,
-    double theta_q,
-    double phi_q,
+    double rq, double theta_q, double phi_q, /*воображаемые координаты несплюснутой сферы*/
     double v0,
     double a,
     double c,
@@ -340,10 +353,11 @@ static double compute_R(
     int use_lorentz_factor,
     int use_lorentz_general_factor
 ) {
-    /* Координаты наблюдателя */
-    double x_a = ra * sin(theta_a) * cos(phi_a);
-    double y_a = ra * sin(theta_a) * sin(phi_a);
-    double z_a = ra * cos(theta_a);
+    /* Координаты наблюдателя в момент t */
+    double x_a, y_a, z_a;
+    get_source_position(t, t0, ra, theta_a, phi_a, v0, a, c, &x_a, &y_a, &z_a,
+        use_lorentz_factor,
+        use_lorentz_general_factor);
 
     /* Координаты источника в момент t' */
     double x_q, y_q, z_q;
@@ -361,6 +375,7 @@ static double compute_R(
 /* Аналитическое вычисление dR/dt' с использованием Лоренц-фактора */
 static double compute_dR_dt_prime_analytical(
     double t_prime,
+    double t,
     double t0,
     double ra,
     double theta_a,
@@ -369,19 +384,20 @@ static double compute_dR_dt_prime_analytical(
     double theta_q,
     double phi_q,
     double v0,
-    double a0,
+    double a,
     double c,
     int use_lorentz_factor,
     int use_lorentz_general_factor
 ) {
-    /* Координаты наблюдателя */
-    double x_a = ra * sin(theta_a) * cos(phi_a);
-    double y_a = ra * sin(theta_a) * sin(phi_a);
-    double z_a = ra * cos(theta_a);
+    /* Координаты наблюдателя в момент t */
+    double x_a, y_a, z_a;
+    get_source_position(t, t0, ra, theta_a, phi_a, v0, a, c, &x_a, &y_a, &z_a,
+        use_lorentz_factor,
+        use_lorentz_general_factor);
 
-    /* Координаты источника в запаздывающий момент времени */
+    /* Координаты источника в запаздывающий момент времени t_prime */
     double x_q, y_q, z_q;
-    get_source_position(t_prime, t0, rq, theta_q, phi_q, v0, a0, c, &x_q, &y_q, &z_q,
+    get_source_position(t_prime, t0, rq, theta_q, phi_q, v0, a, c, &x_q, &y_q, &z_q,
         use_lorentz_factor,
         use_lorentz_general_factor);
 
@@ -392,7 +408,7 @@ static double compute_dR_dt_prime_analytical(
     double R = sqrt(R_x*R_x + R_y*R_y + R_z*R_z);
 
     /* Скорость источника в запаздывающий момент времени */
-    double v = v0 + a0 * (t_prime - t0);
+    double v = v0 + a * (t_prime - t0);
     double gamma = lorentz_factor(v, c);
     double v_r = v / gamma;  /* Радиальная скорость с учетом Лоренц-сокращения */
 
@@ -422,11 +438,13 @@ static double compute_retarded_time_newton(
     int use_lorentz_factor,
     int use_lorentz_general_factor
 ) {
-    /* Начальное приближение методом итераций */
-    double x_a = ra * sin(theta_a) * cos(phi_a);
-    double y_a = ra * sin(theta_a) * sin(phi_a);
-    double z_a = ra * cos(theta_a);
+    /* Координаты наблюдателя в момент t */
+    double x_a, y_a, z_a;
+    get_source_position(t, t0, ra, theta_a, phi_a, v0, a, c, &x_a, &y_a, &z_a,
+        use_lorentz_factor,
+        use_lorentz_general_factor);
 
+    /* Координаты источника в момент времени t */
     double x_q, y_q, z_q;
     get_source_position(t, t0, rq, theta_q, phi_q, v0, a, c, &x_q, &y_q, &z_q,
         use_lorentz_factor, use_lorentz_general_factor);
@@ -459,7 +477,7 @@ static double compute_retarded_time_newton(
             v0, a, c,
             use_lorentz_factor, use_lorentz_general_factor);
         double f = t_prime - t + R_val / c;
-        double df_dt = 1.0 + compute_dR_dt_prime_analytical(t_prime, t0,
+        double df_dt = 1.0 + compute_dR_dt_prime_analytical(t_prime, t, t0,
             ra, theta_a, phi_a,
             rq, theta_q, phi_q,
             v0, a, c,
@@ -478,12 +496,8 @@ static double compute_retarded_time_newton(
 
 /* Функция для вычисления электрического поля с учетом запаздывания */
 static void compute_electric_field_with_delay(
-    double ra,
-    double theta_a,
-    double phi_a,
-    double rq,
-    double theta_q,
-    double phi_q,
+    double ra, double theta_a, double phi_a, /*воображаемые координаты несплюснутой сферы*/
+    double rq, double theta_q, double phi_q,
     const ProblemParams *params,
     double *E1_x, double *E1_y, double *E1_z,
     double *E2_x, double *E2_y, double *E2_z,
@@ -491,17 +505,18 @@ static void compute_electric_field_with_delay(
     double *E_total_y,
     double *E_total_z
 ) {
-    /* Преобразование в декартовы координаты для точки наблюдения */
-    double x_a = ra * sin(theta_a) * cos(phi_a);
-    double y_a = ra * sin(theta_a) * sin(phi_a);
-    double z_a = ra * cos(theta_a);
-
     /* Вычисление запаздывающего времени */
     double t_prime = compute_retarded_time_newton(
         params->t, params->t0, ra, theta_a, phi_a, rq, theta_q, phi_q,
         params->v0, params->a, params->c, 100, 1e-10,
         params->use_lorentz_factor, params->use_lorentz_general_factor
     );
+
+    /* Координаты наблюдателя в момент t */
+    double x_a, y_a, z_a;
+    get_source_position(params->t, params->t0, ra, theta_a, phi_a, params->v0, params->a, params->c, &x_a, &y_a, &z_a,
+        params->use_lorentz_factor,
+        params->use_lorentz_general_factor);
 
     /* Получение координат источника в запаздывающий момент времени */
     double x_q, y_q, z_q;
@@ -548,22 +563,12 @@ static inline cubareal rho_q (cubareal r0, cubareal q)
 }
 
 /* Функция для вычисления плотности заряда с учетом Лоренц-сокращения */
-static inline double rho_q_lorentz(double r0, double q, double v, double c) {
+/*static inline double rho_q_lorentz(double r0, double q, double v, double c) {
     double gamma = lorentz_factor(v, c);
     return gamma * 3.0 * q / (4.0 * M_PI * pow(r0, 3));
-}
+}*/
 
 // интегрирование по координатам заряда источника потенциала
-// E2 - индуктивная компонента массы
-// В приближении малых скоростей ${}^{v} \big / {}_{c}\ll 1$
-// и малых ускорений $a{{r}_{0}}\ll {{c}^{2}}$ и при игнорировании запаздывания
-// Электрическое поле самоиндукции ($z$ компонента)
-/*
-\[{E}_{2}=\\
-\int\limits_{{{r}_{q}}}\int\limits_{{{\varphi}_{q}}}\int\limits_{{{\theta}_{q}}}\\
-{\left\{ -\frac{{a_z}}{{{c}^{2}}{{{R}_{0}}}} \right\}\\
-{\rho \left( {{r}_{q}} \right){{r}_{q}}^{2}\sin \left( {{\theta }_{q}} \right)}\ }d{{\theta }_{q}}d{{\varphi }_{q}}d{{r}_{q}}\]
-*/
 static inline void int_q (const ProblemParams *params, cubareal q, cubareal psi_a, cubareal theta_a, cubareal ra, cubareal psi_q, cubareal theta_q, cubareal rq,
     double *int_q_E1_x, double *int_q_E1_y, double *int_q_E1_z,
     double *int_q_E2_x, double *int_q_E2_y, double *int_q_E2_z,
@@ -595,12 +600,14 @@ static inline void int_q (const ProblemParams *params, cubareal q, cubareal psi_
     }
 
     double rho;
-    if (params->use_lorentz_factor || params->use_lorentz_general_factor)
+    // Если вы интегрируете по "воображаемой" сфере (сферическим координатам без учета сжатия):
+    // Для получения правильного результата вам нужна плотность rho​ , а не gamma * rho
+    /*if (params->use_lorentz_factor || params->use_lorentz_general_factor)
     {
         double v_z = params->v0 + params->a * (params->t - params->t0);
         rho = rho_q_lorentz(r0, q, v_z, params->c);
     }
-    else
+    else*/
     {
         rho = rho_q(r0, q);
     }
@@ -631,12 +638,12 @@ void int_a (
     cubareal r0 = params->R0;
 
     double rho;
-    if (params->use_lorentz_factor || params->use_lorentz_general_factor)
+    /*if (params->use_lorentz_factor || params->use_lorentz_general_factor)
     {
         double v_z = params->v0 + params->a * (params->t - params->t0);
         rho = rho_q_lorentz(r0, q, v_z, params->c);
     }
-    else
+    else*/
     {
         rho = rho_q(r0, q);
     }

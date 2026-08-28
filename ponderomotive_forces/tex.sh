64236f6ed48095ee7de2b053a85fa5486293def9
Alexey Drozdov <daju@yandex.ru>
2026-08-28 13:19:57 +0300

    Tamm_ponderomotive_forces_update.tex update

--- ponderomotive_forces/tex.sh
+++ ponderomotive_forces/tex.sh
@@ -1,3 +1,17 @@
+# brew install --cask basictex
+# sudo tlmgr update --self
+# sudo tlmgr install collection-langcyrillic
+# sudo tlmgr install tcolorbox
+# sudo tlmgr install pdfcol
+# sudo tlmgr install collection-latexextra
+# sudo tlmgr install palatino
+# sudo tlmgr install newpx pxfonts
+# sudo tlmgr install mathpazo
+# sudo updmap-sys
+
+
+
+
 rm *.aux
 rm *.log
 rm *.out
